a0d56ef6598aeb615b09dbc5253af9dabec08f3b
Saurabh Wadhwani <cibiv45073@wlmycn.com>
2023-09-14 20:37:03 +0530

    creating-files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+  return (
+    <>
+        Hello World
+    </>
+  )
+}
+
+export default App
