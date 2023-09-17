815e739e9e7a423f508162ed1485a2c4dcb4304e
sbhwdh98 <145125391+sbhwdh98@users.noreply.github.com>
2023-09-18 05:08:14 +0530

    first-commit

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -17,15 +17,8 @@ const Cards = () => {
                         <span className="">R350 for one</span>
                     </div>
                     <div className="extra"></div>
-
                     {/* 30:30 */}
 
-                    {/* <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button> */}
                 </Card.Body>
             </Card>
         </>
